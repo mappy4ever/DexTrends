79eee44cff5624236b4f62e60871f084f4ce8e9f
mappy4ever <moazzamp@icloud.com>
2025-06-18 15:35:19 -0400

    Update CardList import in [rarity].js

--- pages/cards/rarity/[rarity].js
+++ pages/cards/rarity/[rarity].js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import pokemon from "pokemontcgsdk";
-import CardList from "../../../components/cardlist";
+import CardList from "../../../components/CardList";
 import LoadingSpinner from "../../../components/ui/LoadingSpinner"; // Updated path
 
 const rarityLabelToName = {
